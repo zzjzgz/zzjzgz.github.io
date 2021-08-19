513798e38c11fcbf860dde9a35c67953dd9c9163
zzjzgz <73242033+zzjzgz@users.noreply.github.com>
2021-08-19 14:59:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var dj = document.getElementById('one');
+let mz = document.getElementById('two');
+let lm = document.getElementById('three');
+let sh = document.getElementById('four');
+dj.onclick = function(){
+    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/dj.png)';
+}
+mz.onclick = function(){
+    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/mz.png)';
+}
+lm.onclick = function(){
+    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/lm.png)';
+}
+sh.onclick = function(){
+    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/sh.png)';
+}
