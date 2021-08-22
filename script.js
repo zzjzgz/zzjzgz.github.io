fea0203d48a030eb7cc9df18d609905471eb3316
zzjzgz <73242033+zzjzgz@users.noreply.github.com>
2021-08-22 14:34:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-
-var dj = document.getElementById('one');
-let mz = document.getElementById('two');
-let lm = document.getElementById('three');
-let sh = document.getElementById('four');
-dj.onclick = function(){
-    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/dj.png)';
-}
-mz.onclick = function(){
-    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/mz.png)';
-}
-lm.onclick = function(){
-    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/lm.png)';
-}
-sh.onclick = function(){
-    document.getElementsByTagName('body')[0].style.backgroundImage='url(img/sh.png)';
+var span = document.querySelectorAll('span');
+// console.log(span);
+for (let i = 0;i<span.length;i++){
+    span[0].style.backgroundColor = 'rgba(255, 255,255,1)';
+    span[i].onclick = function(){
+        for (let i = 0;i<span.length;i++){
+            span[i].style.backgroundColor =''
+        }
+        span[i].style.backgroundColor = 'rgba(255, 255,255,1)'
+        // let zzj = document.getElementById('zzj');
+        // let img = document.querySelectorAll('img')[i+1];
+        // zzj.src = img.src;
+        
+        if (i==0){
+            document.getElementsByTagName('body')[0].style.backgroundImage='url(img/dj.png)';
+        }
+        if (i==1){
+            document.getElementsByTagName('body')[0].style.backgroundImage='url(img/mz.png)';
+        }
+        if (i==2){
+            document.getElementsByTagName('body')[0].style.backgroundImage='url(img/lm.png)';
+        }
+        if (i==3){
+            document.getElementsByTagName('body')[0].style.backgroundImage='url(img/sh.png)';
+        }
+    }
 }
