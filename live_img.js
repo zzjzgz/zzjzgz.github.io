ab2adb95f39a8256f4c8ee67e69fefe292f3a290
zzjzgz <73242033+zzjzgz@users.noreply.github.com>
2021-09-23 12:37:55 +0800

    Add files via upload

--- live_img.js
+++ live_img.js
@@ -1,12 +1,14 @@
 let an = document.querySelectorAll(".an");
 let img = document.querySelector(".zhuti").querySelectorAll("img");
 // console.log(an);
-// console.log(img.length);
+// console.log(img);
+let a = document.querySelector(".zhuti").querySelectorAll("a");
 for (let i = 0;i<an.length;i++){
     an[0].style.backgroundColor = 'rgb(255, 255, 255)';
     an[i].onclick = function (){
         for (let j = 0;j<img.length;j++){
             img[j].src = 'img/'+ i + j + '.jpg';
+            a[j].href = 'img/'+ i + j + '.jpg';
         }
         for (let i = 0;i<an.length;i++){
             an[i].style.backgroundColor = '';
